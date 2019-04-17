773d56ae37b596b5b0a594ff4f6a44134e972002
Bogdankozlovskiy <bkozlovsky@bk.ru>
2019-04-17 19:34:01 +0300

    just commit

--- video/views.py
+++ video/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Video, Comment
+from django.template.context_processors import csrf
 
 
 def hello(request):
@@ -8,4 +9,19 @@ def hello(request):
     comment = Comment.objects.all()
     return render(request, 'index.html', {"video": video, 'comment':comment})
     #return HttpResponse('<h1>hello world</h1>')
+
+
+def fun(request):
+    if request.GET:
+        print(request.user.username)
+        print(request.GET['login'])
+        print(request.GET['password'])
+        return redirect('/hello/')
+    if request.POST:
+        print(request.user.username)
+        print(request.POST['login'])
+        print(request.POST['password'])
+        return redirect('/hello/')
+    return render(request, "forms.html", csrf(request))
+
 # Create your views here.
